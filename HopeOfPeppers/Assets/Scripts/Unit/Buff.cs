1e176306641be6906f12963c9696fa75aad37f21
Hanmingy <bmind2001@naver.com>
2018-01-27 13:19:53 +0900

    ㅗㅡㅏ 쥬

---
+++ HopeOfPeppers/Assets/Scripts/Unit/Buff.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff
+{
+    public int kind;
+    public float buffPower;
+    public float buffHp;
+    public float buffMoveSpeed;
+    public float buffAttackSpeed;
+    public float buffRange;
+    public int buffRemainTime;
+}
+
+
+public class BuffManager
+{
+    private static BuffManager instance = null;
+    public Hashtable buffHashTable;
+
+    public static BuffManager Instance
+    {
+        get
+        {
+            if (instance==null)
+            {
+                instance = new BuffManager();
+            }
+            return instance;
+        }
+    }
+
+    private BuffManager ()
+    {
+        buffHashTable = new Hashtable();
+    }
+
+    public void addBuff(Unit target, Buff buff)
+    {
+        if(buffHashTable.ContainsKey(target))
+        {
+            List<Buff> tmp = buffHashTable[target] as List<Buff>;
+            tmp.Add(buff);
+        }
+        else
+        {
+            List<Buff> tmp = new List<Buff>();
+            tmp.Add(buff);
+            buffHashTable.Add(target, tmp);
+        }        
+    }
+    public void calculateBuff()
+    {
+    }
+    public void deleteTarget(Unit target)
+    {
+        if(buffHashTable.ContainsKey(target))
+        {
+            buffHashTable.Remove(target);
+        }
+    }
+    
+}
---
+++ HopeOfPeppers/Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat
+{
+    public int level{get; private set;}
+    public int power{get; private set;}
+    public int hp{get; private set;}
+    public int moveSpeed{get; private set;}
+    public int attackSpeed{get; private set;}
+    public int range {get; private set;}
+    
+    public Stat(int aLevel = 0, int aPower = 0, int aHp = 0, int aMoveSpeed = 0, int aAttackSpeed = 0, int aRange = 0)
+    {
+        level = aLevel;
+        power = aPower;
+        hp = aHp;
+        moveSpeed = aMoveSpeed;
+        attackSpeed = aAttackSpeed;
+        range = aRange;
+    }
+}
+
+public class Unit {
+    private int id;
+    private int kind;
+    private Stat stat;
+    private Stat buffedStat;
+    private Vector3 position;
+    private Vector3 direction;
+    
+    public static bool operator ==(Unit aUnit, Unit bUnit)
+    {
+        return aUnit.id == bUnit.id;
+    }
+    public static bool operator !=(Unit aUnit, Unit bUnit)
+    {
+        return aUnit.id != bUnit.id;
+    }
+    public override bool Equals(object obj)
+    {
+        var item = obj as Unit;
+
+        if (item == null)
+        {
+            return false;
+        }
+
+        return this.id.Equals(item.id);
+    }
+    public override int GetHashCode()
+    {
+        return this.id.GetHashCode();
+    }
+}
+
+
+public class UnitManager
+{
+    private static UnitManager instance = null;
+    public Hashtable unitHashTable;
+
+    public static UnitManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new UnitManager();
+            }
+            return instance;
+        }
+    }
+
+    private UnitManager()
+    {
+        unitHashTable = new Hashtable();
+    }
+
+    public void addUnit(int campId, Unit unit)
+    {
+        
+    }
+}
