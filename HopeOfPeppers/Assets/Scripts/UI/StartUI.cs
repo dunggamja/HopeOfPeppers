059d0cb71c7c7d8a5df64a678eaba1c0bc338d5e
dunggamja <dunggamja@gmail.com>
2018-01-29 21:41:56 +0900

    UI 코드  & 프리팹 추가

---
+++ HopeOfPeppers/Assets/Scripts/UI/StartUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartUI : UI_Base
+{
+    public Button buttonStart = null;
+    public Image imageStart = null;
+
+    private void Start()
+    {
+        buttonStart.onClick.AddListener(Click_Start);
+    }
+
+
+    private void Click_Start()
+    {
+        //Debug.Log("ClickStart");
+        GameMain.Instance.StageMgr.LoadLevel(1);
+    }
+
+}
---
+++ HopeOfPeppers/Assets/Scripts/UI/UI_Base.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Base : MonoBehaviour
+{
+
+    public virtual void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Close()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
